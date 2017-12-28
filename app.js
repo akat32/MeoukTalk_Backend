becaaec864e1d9c020b828225b20ab012ad26218
akat32 <baduk32127@gmail.com>
2017-12-28 21:18:37 +0900

    fuck!

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ const rndstring = require('randomstring');
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const CORS = require('cors')();
+const path = require('path');
+app.use(CORS);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({limit: '1gb', extended: false }));
 require('./mongo');
 
+var port = process.env.PORT || 4000;
 var auth = require('./routes/auth')(express.Router(), Users, rndstring);
 var friend = require('./routes/friend')(express.Router(), Users);
 var chat = require('./routes/chat')(express.Router(),Users,io);
+app.set('views', path.join(__dirname, 'views'));
+app.set('port', port)
+app.set('view engine', 'ejs');
 
 app.use('/friend',friend);
 app.use('/auth', auth);
 app.use('/chat', chat);
+
+app.get('/', (req,res)=>{
+  res.render('main');
+})
 http.listen(3321, (req,res)=>{
   console.log('Server Porting on 3321');
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 module.exports = (router,Users,rndstring)=>{
   router.post('/signin', async (req,res)=>{
-    let result = await Users.findOne(req.body);
+    const result = await Users.findOne(req.body);
     if(!result) return res.status(404).json({message : "Users Not found"});
     else{
       let token = result.token;
@@ -16,14 +16,14 @@ module.exports = (router,Users,rndstring)=>{
     res.status(200).json({message : "signup success!"});
   })
   .get('/signout/:token', async (req,res)=>{
-    let token = req.params.token;
+    const token = req.params.token;
     var result = await Users.update({token : token}, {$set:{isLogined : false}});
     if(result.ok) return res.status(200).json({message : "Logout success!"});
     else return res.stauts(500).json({message : "Logout failed"});
   })
   .get('/auto/:token', async (req,res)=>{
-    let token = req.params.token;
-    let result = await Users.findOne({token : token});
+    const token = req.params.token;
+    const result = await Users.findOne({token : token});
     if(result.isLogined == 1) return res.status(200).json(result);
     else if(!result) return res.status(404).json({message : "Users Not found"});
     else return res.status(401).json({message : "User is not logined"});
